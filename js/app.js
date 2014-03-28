d5cc1f918aece8a65ea7a051afe663a41535447c
Nik <nik@khilnani.org>
2014-03-28 16:37:41 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 
-
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
 function encrypt(text, pass) {
   //console.log('pass:' + pass + ' encrypt IN:' + text);
   var key = str_sha1(pass);  
-  var encrypted = Aes.Ctr.encrypt(text, key,128);
+  var encrypted = Aes.Ctr.encrypt(text, key, 256);
   //console.log('encrypt OUT:' + encrypted);
   return encrypted;
 }
@@ -13,7 +12,7 @@ function encrypt(text, pass) {
 function decrypt (text, pass) {
   //console.log('pass:' + pass + ' decrypt IN:' + text);
   var key = str_sha1(pass);  
-  var decrypted = Aes.Ctr.decrypt(text, key,128);
+  var decrypted = Aes.Ctr.decrypt(text, key, 256);
   //console.log('decrypt OUT:' + decrypted);
   return decrypted;
 }
