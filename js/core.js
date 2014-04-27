5ed5c672b2a47fa1f71e8ebb6085bb202d64c334
Nik <nik@khilnani.org>
2014-04-26 22:26:14 -0400

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,16 +1,3 @@
-function getUrlVars()
-{
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-}
-
 function encrypt(text, pass) {
   //console.log('pass:' + pass + ' encrypt IN:' + text);
   var key = Sha256.hash(pass);  
