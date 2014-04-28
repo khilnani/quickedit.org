3dcecd24e0205635e299bfa2c6fbbfd6d2c216b6
Nik <nik@khilnani.org>
2014-04-27 23:56:04 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 require(['require-config'], function(config) {
 
   console.log("main.");
-  
   require.config(config);
   
   require(['views/mainview'], function(MainView) {
-    
       console.log("main()");
       mainView = new MainView();
-
     }
   );
 });
