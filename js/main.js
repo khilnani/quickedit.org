a39869f7b6c18ba55fcdca2f24f41d14ea28e796
Nik <nik@khilnani.org>
2014-04-29 08:10:21 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ require(['require-config'], function(config) {
   require(['views/mainview'], function(MainView) {
       console.log("main()");
       var mainView = new MainView();
+      $('#splashscreen').delay( 1000 ).fadeOut('slow');
     }
   );
 });
