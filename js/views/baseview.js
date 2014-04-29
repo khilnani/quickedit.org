ff8dea7ce528f661d263abe7bfc7e689d768e11c
Nik <nik@khilnani.org>
2014-04-28 20:28:06 -0400

    Update baseview.js

--- js/views/baseview.js
+++ js/views/baseview.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore', 'backbone', 'globals/utils', 'globals/eventbus', 'jquery.autosize', 'sha256', 'aes', 'bootstrap', 'bootstrap.mods', 'bootbox'], function($, _, Backbone, Utils, EventBus) {
+define(['jquery', 'underscore', 'backbone', 'globals/utils', 'globals/eventbus', 'jquery.autosize', 'bootstrap', 'bootstrap.mods', 'bootbox'], function($, _, Backbone, Utils, EventBus) {
   "use strict";
   
   console.log("BaseView.");
