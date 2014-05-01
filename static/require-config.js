5c18de4bac1ba04284b92550b09e41084380eced
Nik <nik@khilnani.org>
2014-05-01 00:37:59 -0400

    Update require-config.js

--- static/require-config.js
+++ static/require-config.js
@@ -14,6 +14,7 @@ define('require-config',[], function() {
       "jquery": "libs/jquery/jquery-2.1.0.min",
       "jquery.autosize": "libs/jquery/plugins/jquery.autosize.min",
       "bootstrap": "libs/bootstrap/bootstrap-3.1.1.min",
+      "bootstrap.mods": "mods/bootstrap.mods",
       "bootbox": "libs/bootbox/bootbox.min",
       "underscore": "libs/underscore/underscore-1.6.0.min",
       "backbone": "libs/backbone/backbone-1.1.2.min",
