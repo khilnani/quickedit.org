edd69f7caab461b35ccb9b500396ed51282b349b
khilnani <nik@khilnani.org>
2014-05-04 09:01:32 -0400

    Read files

--- static/modules/cloudstore.js
+++ static/modules/cloudstore.js
@@ -3,36 +3,43 @@ define(['jquery', 'bootbox', 'dropbox'], function ($, bootbox) {
   console.log('CloudStore.');
   
   var CloudStore = window.CloudStore = {  
-  
+
+    currentDir: '/',
+    deferred: undefined,
+
     loadFile: function () {
       console.log("CloudStore.loadFile");
-      var deferred = $.Deferred();
+      CloudStore.deferred = $.Deferred();
       
       CloudStore.client.authenticate( function( error, data ) {
         console.log("CloudStore.loadFile/authenticate: error: " + error);
         if(error) {
-          deferred.reject();
+          CloudStore.deferred.reject();
         } else {
-          CloudStore.client.getAccountInfo( function( error, userInfo ) {
-            console.log("CloudStore.loadFile/getAccountInfo: error: " + error);
-            if(error) {
-              deferred.reject();
-            } else {
-              console.log("CloudStore.loadFile userInfo.name:" + userInfo.name );
-              deferred.resolve( userInfo.name );
-            }
-          })
+          CloudStore.readDir();
         }
       });
       console.log("CloudStore.loadFile return deferred");
-      return deferred;
+      return CloudStore.deferred;
     },
     
     saveFile: function (text) {
       console.log("CloudStore.saveFile");
-      var deferred = $.Deferred();
-      deferred.resolve();
-      return deferred;
+      CloudStore.deferred = $.Deferred();
+      CloudStore.deferred.resolve();
+      return CloudStore.deferred;
+    },
+
+    readDir: function () {
+      CloudStore.client.readdir( CloudStore.currentDir, function( error, entries ) {
+        console.log("CloudStore.readDir/readdir: '" + CloudStore.currentDir + "' error: " + error);
+        if(error) {
+          CloudStore.deferred.reject();
+        } else {
+          console.log("CloudStore.readDir entries: " +  entries.length );
+          CloudStore.deferred.resolve( entries.length );
+        }
+      });
     }
   };
   
