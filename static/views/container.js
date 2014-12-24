5b8e17713d345a59b45ae5211bfca9e96f794fbd
Nik <nik@khilnani.org>
2014-12-24 01:24:34 -0500

    Update container.js

--- static/views/container.js
+++ static/views/container.js
@@ -187,7 +187,7 @@ function($, _, BaseView, EventBus, bootbox, CloudStore, CodeMirror) {
       if( ! isiOS() ) {
         console.log('Initializing CodeMirror.');
         
-        $('#keyMapVim').removeClass('hidden').fadeIn();
+        $('#keyMapVim').removeClass('hidden');
         
         this.editor = CodeMirror.fromTextArea(document.getElementById("message"), {
           lineNumbers: true,
