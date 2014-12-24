095e8dc4a19b45775a79c6356db1ff7bbe5a09db
Nik <nik@khilnani.org>
2014-12-24 00:08:15 -0500

    Update container.js

--- static/views/container.js
+++ static/views/container.js
@@ -1,5 +1,5 @@
 define([
-  'jquery', 'underscore', 'views/base', 'globals/eventbus', 'bootbox', 'modules/cloudstore', 'ace', 'codemirror', 'cm.vim',
+  'jquery', 'underscore', 'views/base', 'globals/eventbus', 'bootbox', 'modules/cloudstore', 'ace', 'codemirror', 'cm.vim', 'cm.clike',
   'css!libs/codemirror/codemirror-4.8.0.css', 'cm.placeholder',
   'add2home', 'css!libs/add2home/add2home.css', 'sha256', 'aes'], 
 function($, _, BaseView, EventBus, bootbox, CloudStore, ace, CodeMirror) {
