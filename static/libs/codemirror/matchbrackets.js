23463e2df6296d7d192821a7aedce905ed663ade
Nik <nik@khilnani.org>
2014-12-23 23:54:06 -0500

    Update matchbrackets.js

--- static/libs/codemirror/matchbrackets.js
+++ static/libs/codemirror/matchbrackets.js
@@ -1,14 +1,11 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
+define(['codemirror'], 
+function(CodeMirror) {
+  "use strict";
+  console.log("matchbrackets.");
+  
   var ie_lt8 = /MSIE \d/.test(navigator.userAgent) &&
     (document.documentMode == null || document.documentMode < 8);
 
