136ac2a24c15156b00a58ef9360f3c92ba860318
Nik <nik@khilnani.org>
2014-12-23 23:53:48 -0500

    Update dialog.js

--- static/libs/codemirror/dialog.js
+++ static/libs/codemirror/dialog.js
@@ -3,14 +3,11 @@
 
 // Open simple dialogs on top of an editor. Relies on dialog.css.
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
+define(['codemirror'], 
+function(CodeMirror) {
+  "use strict";
+  console.log("dialog.");
+
   function dialogDiv(cm, template, bottom) {
     var wrap = cm.getWrapperElement();
     var dialog;
