d4ea414822a16649687e9eeec6d6edbce8f04c92
khilnani <nik@khilnani.org>
2015-10-14 19:59:49 -0400

    commented out backtop that causes scrolling issue when textfield shrinks

--- static/views/container.js
+++ static/views/container.js
@@ -1,18 +1,18 @@
 define([
-  'jquery', 'underscore', 'globals/localstorage','globals/utils', 'views/base', 'globals/eventbus', 'bootbox', 'modules/cloudstore', 
-  'libs/codemirror/lib/codemirror', 
-  
+  'jquery', 'underscore', 'globals/localstorage','globals/utils', 'views/base', 'globals/eventbus', 'bootbox', 'modules/cloudstore',
+  'libs/codemirror/lib/codemirror',
+
   'css!libs/codemirror-mods/codemirror.css',
   'css!libs/codemirror/theme/eclipse.css',
-  'css!libs/codemirror/theme/lesser-dark.css', 
-  'css!libs/codemirror/theme/mbo.css', 
-  'css!libs/codemirror/theme/mdn-like.css', 
-  'css!libs/codemirror/theme/monokai.css', 
-  'css!libs/codemirror/theme/paraiso-dark.css', 
-  'css!libs/codemirror/theme/solarized.css', 
-  'css!libs/codemirror/theme/twilight.css', 
-  'css!libs/codemirror/theme/zenburn.css', 
-  
+  'css!libs/codemirror/theme/lesser-dark.css',
+  'css!libs/codemirror/theme/mbo.css',
+  'css!libs/codemirror/theme/mdn-like.css',
+  'css!libs/codemirror/theme/monokai.css',
+  'css!libs/codemirror/theme/paraiso-dark.css',
+  'css!libs/codemirror/theme/solarized.css',
+  'css!libs/codemirror/theme/twilight.css',
+  'css!libs/codemirror/theme/zenburn.css',
+
   'libs/codemirror/mode/coffeescript/coffeescript',
   'libs/codemirror/mode/commonlisp/commonlisp',
   'libs/codemirror/mode/clike/clike',
@@ -47,46 +47,46 @@ define([
   'libs/codemirror/mode/yaml/yaml',
   'libs/codemirror/keymap/vim', 'libs/codemirror/addon/display/placeholder',
   'libs/codemirror/addon/edit/matchbrackets', 'libs/codemirror/addon/search/searchcursor', 'libs/codemirror/addon/dialog/dialog',
-  'add2home', 'css!libs/add2home/add2home.css', 'sha256', 'aes'], 
+  'add2home', 'css!libs/add2home/add2home.css', 'sha256', 'aes'],
 function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, CodeMirror) {
   "use strict";
-  
+
   console.log("ContainerView.");
-  
+
   var ContainerView = BaseView.extend({
-  
+
     el: $('#container'),
     currentFolder: undefined,
     currentFile: undefined,
     editor: undefined,
-    
+
     events: {
       "keyup #password": "passwordsMatch",
       "keyup #password2": "passwordsMatch",
-      
+
       "click #encrypt": "encryptMessage",
       "click #decrypt": "decryptMessage",
-      
+
       "click #message": "refreshMessage",
       "keyup #message": "refreshMessage",
       "click #clearMessage": "clearMessage",
-      
+
       "click #dbChooseFile": "readFile",
       "click #dbSaveFile": "saveFile",
-      
+
       "click #backToTop": "backToTop",
       "click #logout": "logout",
-      
+
       "click #keyMapVim": "toggleKeyMapVim",
       "click .cm-select-mode": "selectMode",
       "click .cm-select-theme": "selectTheme"
-      
+
     },
-    
+
     logout: function () {
       CloudStore.logout();
     },
-    
+
     _clearMessage: function () {
       console.log('_clearMessage()');
       $('#message').val('\n\n\n\n\n\n\n');
@@ -103,12 +103,12 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
         $('#filePath').html( 'New' );
       }
     },
-    
+
     readFile: function () {
       console.group("readFile");
       var promise = CloudStore.readFile( this.currentFolder );
       var self = this;
-      
+
       promise.done( function( text, location, fileName ) {
         console.log("read: " + location.join('/') + ', ' + fileName);
         self.displayFileInfo( location, fileName);
@@ -121,13 +121,13 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
         console.groupEnd();
       });
     },
-    
+
     saveFile: function () {
       console.log("saveFile");
 
       var self = this;
       var promise = CloudStore.saveFile( $('#message').val(), this.currentFolder );
-      
+
       promise.done( function( location, fileName) {
         console.log("saved: " + location.join('/') + ', ' + fileName);
         self.displayFileInfo( location, fileName);
@@ -138,23 +138,23 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
         console.groupEnd();
       });
     },
-    
+
     encrypt: function (text, pass) {
       //console.log('pass:' + pass + ' encrypt IN:' + text);
-      var key = Sha256.hash(pass);  
+      var key = Sha256.hash(pass);
       var encrypted = Aes.Ctr.encrypt(text, key, 256);
       //console.log('encrypt OUT:' + encrypted);
       return encrypted;
     },
-    
+
     decrypt: function (text, pass) {
       //console.log('pass:' + pass + ' decrypt IN:' + text);
-      var key = Sha256.hash(pass);  
+      var key = Sha256.hash(pass);
       var decrypted = Aes.Ctr.decrypt(text, key, 256);
       //console.log('decrypt OUT:' + decrypted);
       return decrypted;
     },
-    
+
     encryptMessage: function() {
       console.group("encryptMessage()");
       if ( this.passwordsMatch() ) {
@@ -163,25 +163,25 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
       }
       console.groupEnd();
     },
-    
+
     decryptMessage: function () {
       console.group("decryptMessage()");
-      if( this.passwordsMatch() ) {  
+      if( this.passwordsMatch() ) {
         $('#message').val( this.decrypt( $('#message').val(), $('#password').val() ) );
         EventBus.trigger('message:updated');
       }
       console.groupEnd();
     },
-    
+
     refreshMessage: function () {
       console.log("refreshMessage()");
-      
+
       var m = $('#message');
       $("#count").text( m.val().length );
       m.autosize({ append: '\n'});
       m.trigger('autosize.resize');
     },
-    
+
     clearMessage: function () {
       var self = this;
       bootbox.confirm("Clear message?", function(result) {
@@ -193,10 +193,10 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
         }
       });
     },
-    
+
     passwordsMatch: function () {
       console.log("passwordsMatch()");
-  
+
       if( $('#password').val() == $('#password2').val() ) {
         $('#passGroup').removeClass("has-error");
         $('#passwordError').addClass("hidden");
@@ -205,19 +205,19 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
 
       $('#passGroup').addClass("has-error");
       $('#passwordError').removeClass("hidden");
-  
-      this.backToTop();
+
+      //this.backToTop();
 
       return false;
     },
-    
+
     backToTop: function () {
       $("html, body").animate({ scrollTop: 0 }, "slow");
     },
-    
+
     toggleKeyMapVim: function () {
       if(this.editor.getOption("keyMap") == "vim") {
-        this.editor.setOption("keyMap","default");  
+        this.editor.setOption("keyMap","default");
         $('#keyMapVim').html('Mode: Vim');
         localStorage.setItem('keymap_value', "default");
         localStorage.setItem('keymap_label', 'Mode: Vim');
@@ -228,7 +228,7 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
         localStorage.setItem('keymap_label', '<strong>Mode: Vim</strong>');
       }
     },
-    
+
     selectMode: function (event) {
       var id = $(event.target).data('id');
       var mode = $(event.target).data('mode');
@@ -242,7 +242,7 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
       localStorage.setItem('mode_value', mode);
       localStorage.setItem('mode_label', $(event.target).html());
     },
-    
+
     selectTheme: function (event) {
       var id = $(event.target).data('id');
       console.log("selectTheme(): " + id);
@@ -251,20 +251,20 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
       localStorage.setItem('theme_value', id);
       localStorage.setItem('theme_label', $(event.target).html());
     },
-  
+
     initialize: function(options) {
       console.log("ContainerView()");
-      
+
       BaseView.prototype.initialize.call(this, options);
-  
+
       var self = this;
-      
+
       if( ! Utils.isMobile() ) {
         console.log('Initializing CodeMirror.');
-        
+
         this._clearMessage();
         $('.desktop').removeClass('hidden');
-        
+
         this.editor = CodeMirror.fromTextArea(document.getElementById("message"), {
           lineNumbers: true,
           styleActiveLine: true,
@@ -275,45 +275,45 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
           //mode: "text/x-csrc",
           keyMap: "default"
         });
-        
+
         this.editor.on("change", function(editor, change) {
           editor.save();
           self.refreshMessage();
         });
-        
+
         this.editor.on('keypress', function(editor, e) {
           //console.log(e.keyCode);
         });
-        
+
         // set prior session config
         var keymap_value = localStorage.getItem('keymap_value');
         var keymap_label = localStorage.getItem('keymap_label');
         if(keymap_value && keymap_label) {
-          this.editor.setOption("keyMap",keymap_value); 
+          this.editor.setOption("keyMap",keymap_value);
           $('#keyMapVim').html(keymap_label);
         }
-        
+
         var mode_value = localStorage.getItem('mode_value');
         var mode_label = localStorage.getItem('mode_label');
         if(mode_value && mode_label) {
           this.editor.setOption('mode', mode_value);
           $('#cm-select-mode-btn').html( mode_label );
         }
-        
+
         var theme_value = localStorage.getItem('theme_value');
         var theme_label = localStorage.getItem('theme_label');
         if (theme_value && theme_label) {
           this.editor.setOption('theme', theme_value);
           $('#cm-select-theme-btn').html( theme_label );
         }
-        
+
       }
-  
+
       this.refreshMessage();
 
       EventBus.on('message:updated', function(){
         console.log('message:updated');
-        
+
         if( ! Utils.isMobile() ) {
           this.editor.setValue( $('#message').val() );
         }
@@ -321,7 +321,7 @@ function($, _, localStorage, Utils, BaseView, EventBus, bootbox, CloudStore, Cod
         this.refreshMessage();
       }, this);
     },
-    
+
     destroy: function() {
       EventBus.off('message:updated');
       BaseView.prototype.destroy.call(this);
